0fa5149c501c27d501ecf3216a86c91d1a799b08
Shourabh Singh <sourav.singh28@gmail.com>
2020-11-23 02:11:18 +0530

    Update buildHelper.groovy

--- buildHelper.groovy
+++ buildHelper.groovy
@@ -6,10 +6,7 @@ def buildvars() {
         ARTIFACT_REPOSITORY_LOCAL = 'pfi-libs-snapshot-local'
         ARTIFACT_REPOSITORY_RELEASE = 'pfi-libs-release-local'
         REPO_PATH = '/com/prudential/fi/microservices'
-        ARTIFACTORY_URL = 'https://artifactory.prudential.com/artifactory/'
-        deploy_job_develop = 'deployment'
-        deploy_job_release = 'prod-deployment'
-        deploy_bcjob_release = 'bc-deployment'
+        ARTIFACTORY_URL = 'artifactory.prudential.comartifactory'
 
     ]
 }
